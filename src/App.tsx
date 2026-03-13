1063df8ce399b3f0a1ec7226af9d5c3655281ad3
ReBoho <catarinarebocho30@gmail.com>
2026-03-13 17:20:10 +0000

    Remoção de ficheiros não usados

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,6 @@ import { AuthProvider } from "@/contexts/AuthContext";
 import Index from "./pages/Index";
 import Auth from "./pages/Auth";
 import Backoffice from "./pages/Backoffice";
-import Debug from "./pages/Debug";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -25,7 +24,6 @@ const App = () => (
               <Route path="/" element={<Index />} />
               <Route path="/auth" element={<Auth />} />
               <Route path="/backoffice" element={<Backoffice />} />
-              <Route path="/debug" element={<Debug />} />
               {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
               <Route path="*" element={<NotFound />} />
             </Routes>
